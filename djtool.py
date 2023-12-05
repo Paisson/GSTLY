2cc9be071a1fb5a42a26c93ff0d4b3c0adbf3f82
Jacob <jacob.schueler@hotmail.com>
2023-12-05 23:30:42 +0100

    MWP

--- djtool.py
+++ djtool.py
@@ -1,6 +1,7 @@
-from DJToolGUI import DJToolGUI
+from djtoolgui import DJToolGUI
 import tkinter as tk
 import subprocess
+import threading
 
 
 class DJTool:
@@ -10,25 +11,25 @@ class DJTool:
         self.allow_playlist = tk.BooleanVar()
         self.allow_playlist.set(False)
 
-    # Runs the command and prints result
-    def run_command(self, command):
-        result = subprocess.run(command, capture_output=True, text=True)
-        print(result.stdout)
+        self.thread_completion_event = threading.Event()
 
-    # Start download with max_downloads
+    def run_command(self, command):
+        subprocess.run(command, capture_output=True, text=True)
+
     def download_video(self, search_query):
         if self.allow_playlist.get() is False:
-            if self.max_downloads > 0: 
+            if self.max_downloads > 0:
                 command = f'yt-dlp -P {self.download_path} --no-playlist --add-metadata --embed-thumbnail --match-filter "duration < 1800" --max-downloads {self.max_downloads} {search_query}'
             else:
                 command = f'yt-dlp -P {self.download_path} --no-playlist --add-metadata --embed-thumbnail --match-filter "duration < 1800" {search_query}'
-        else: 
-            if self.max_downloads > 0: 
+        else:
+            if self.max_downloads > 0:
                 command = f'yt-dlp -P {self.download_path} --add-metadata --embed-thumbnail --match-filter "duration < 1800" --max-downloads {self.max_downloads} {search_query}'
             else:
                 command = f'yt-dlp -P {self.download_path} --add-metadata --embed-thumbnail --match-filter "duration < 1800" {search_query}'
-        self.run_command(command)   
-        print('Done') 
+
+        self.run_command(command)
+        self.thread_completion_event.set()
 
 
 if __name__ == "__main__":
--- djtoolgui.py
+++ djtoolgui.py
@@ -1,12 +1,11 @@
 from tkinter import filedialog, simpledialog, ttk
 import tkinter as tk
-
+import threading
 
 class DJToolGUI:
     def __init__(self, tool, root):
         self.tool = tool
         self.root = root
-
         self.root.title("DJ Tool")
 
         # Create a notebook
@@ -39,9 +38,12 @@ class DJToolGUI:
         self.submit_button = tk.Button(self.tab1, text="Start", command=self.submit)
         self.max_downloads_button = tk.Button(self.tab1, text=f'Max: {self.tool.max_downloads}', command=self.set_max_downloads)
 
+        # Create Info Label
+        self.info_label = tk.Label(self.tab1, text='Working')
+
         # Pack the widgets in the new order
         self.link_label.grid(row=0, column=0, pady=10, sticky=tk.E)
-        self.link_entry.grid(row=0, column=1, pady=10, padx=5,sticky=tk.W)
+        self.link_entry.grid(row=0, column=1, pady=10, padx=5, sticky=tk.W)
         self.browse_button.grid(row=0, column=2, pady=10, padx=5, sticky=tk.W)
 
         self.track_label.grid(row=1, column=0, pady=10, sticky=tk.E)
@@ -50,7 +52,6 @@ class DJToolGUI:
         self.max_downloads_button.grid(row=2, column=0, pady=10, padx=5, sticky=tk.E)
         self.playlist_checkbox.grid(row=2, column=1, pady=10, padx=5)
         self.submit_button.grid(row=2, column=2, pady=10, padx=5, sticky=tk.E)
-
 
     def set_max_downloads(self):
         self.tool.max_downloads = simpledialog.askinteger('Set max Songs', prompt='Enter the maximum number of songs to download:')
@@ -61,10 +62,41 @@ class DJToolGUI:
         self.tool.download_path = filedialog.askdirectory()
         print(f'Path set to {self.tool.download_path}')
 
+    def download_thread_callback(self, thread_id, query):
+        self.info_label['text'] = f'Thread {thread_id} finished: {query}'
+
+    def download_worker(self, query):
+        thread_id = threading.current_thread().ident
+        self.tool.download_video(query)
+        self.download_thread_callback(thread_id, query)
+
+    def check_thread_completion(self):
+        if self.tool.thread_completion_event.is_set():
+            self.info_label['text'] = 'Finished'
+            self.tool.thread_completion_event.clear()
+        else:
+            self.root.after(100, self.check_thread_completion)
+
     def submit(self):
-        # Accessing the values entered in the fields
         search_query = self.text_entry.get("1.0", tk.END)
         link = self.link_entry.get()
 
-        self.tool.download_video(search_query) if search_query != '\n' else None
-        self.tool.download_video(link) if link != '\n' else None
+        self.info_label.grid(row=3, column=1, pady=10)
+
+        threads = []
+
+        if search_query != '\n':
+            thread = threading.Thread(target=self.download_worker, args=(search_query,))
+            thread.start()
+            threads.append(thread)
+
+        if link != '\n':
+            thread = threading.Thread(target=self.download_worker, args=(link,))
+            thread.start()
+            threads.append(thread)
+
+        self.root.after(100, self.check_thread_completion)
+            
+
+
+
